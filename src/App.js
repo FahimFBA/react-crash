2ea21c92e8bc400d596c349765cbe722b21f113e
FahimFBA <fahimbinamin@gmail.com>
2023-06-23 19:11:06 +0600

    props

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 import "./App.css";
 
+const Person = (props) => {
+  return (
+    <>
+      <h1>Name: {props.name} </h1>
+      <h1>Last Name: {props.lastName} </h1>
+      <h1>Age: {props.age} </h1>
+    </>
+  );
+};
+
 const App = () => {
   const name = "John";
   const isNameShowing = true;
   return (
     <div className="App">
-      <h1>Hello {2 + 2} !</h1>
-      {name ? (
-        <>
-          test
-        </>
-      ) : (
-        test
-      )
-
-      }
+      <Person name={"John"} lastName={"Doe"} age={25} />
+      <Person name={"Fahim"} lastName={"Amin"} age={24} />
     </div>
   );
 };
