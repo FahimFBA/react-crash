82d48569246fc562fad1760def186b0306d85d5e
FahimFBA <fahimbinamin@gmail.com>
2023-06-23 19:18:16 +0600

    react counter

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
+import { useState } from "react";
 import "./App.css";
 
-const Person = (props) => {
-  return (
-    <>
-      <h1>Name: {props.name} </h1>
-      <h1>Last Name: {props.lastName} </h1>
-      <h1>Age: {props.age} </h1>
-    </>
-  );
-};
-
 const App = () => {
-  const name = "John";
-  const isNameShowing = true;
+  const [counter, setCounter] = useState(0);
+
   return (
     <div className="App">
-      <Person name={"John"} lastName={"Doe"} age={25} />
-      <Person name={"Fahim"} lastName={"Amin"} age={24} />
+      <button onClick={() => setCounter((prevCount) => prevCount - 1)}>
+        -
+      </button>
+      <h1>{counter}</h1>
+      <button onClick={() => setCounter((prevCount) => prevCount + 1)}>
+        +
+      </button>
     </div>
   );
 };
